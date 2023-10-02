1a2040ae269e036071142c7676a34470078f8272
Pradyumancoder <shuklapradyuman786@gmail.com>
2023-10-02 13:03:03 +0530

    project-started

---
+++ src/components/Car.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Car() {
+  return (
+    <div>
+        <h1>car home page</h1>
+       <div className='h-[800px] w-[100%] border border-black m-auto'>
+            <div>
+                <Navbar/>
+            </div>
+       </div>
+    </div>
+  )
+}
+
+export default Car
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+       <div className='h-[50px] w-[90%] border border-red-600'>
+        <input type="week" />
+           <button>Revelence</button>
+           <button>All Brand</button>
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
