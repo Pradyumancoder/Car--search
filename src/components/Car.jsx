e2f901cae33602347a1346eb06ff8ad096bd2062
Pradyumancoder <shuklapradyuman786@gmail.com>
2023-10-02 18:40:42 +0530

    navbar-page-created

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -5,8 +5,8 @@ function Car() {
   return (
     <div>
         <h1>car home page</h1>
-       <div className='h-[800px] w-[100%] border border-black m-auto '>
-        0
+       <div className='h-[800px] w-[100%] border border-black m-auto bg-[#EDEFF6] '>
+        
             <div>
                 <Navbar/>
 
